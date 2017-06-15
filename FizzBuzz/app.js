f3eeb9454d3cbecda831ebee59b36530a0cf6ab8
Kristoffer Panahon <krp312@me.com>
2017-06-15 17:13:03 -0400

    fizzbuzz solving

--- FizzBuzz/app.js
+++ FizzBuzz/app.js
@@ -2,13 +2,21 @@ function fizzBuzzer() {
 	$("#number-chooser").submit(function(event){
 		event.preventDefault();
 		let userNumber = $(event.currentTarget).find("#number-choice").val();
-		console.log(userNumber);
-		for (let i = 1; i < userNumber + 1; i++) {
-			$(".js-results").append(`<div class="fizz-buzz-item"><span>${i}</span></div>`)
-			
-
+		for (let i = 1; i <= userNumber; i++) {
+			if (i % 5 === 0) {
+				$(".js-results").append(`<div class="fizz-buzz-item buzz"><span>${i}</span></div>`);
+			} 
+			else if (i % 3 === 0) {
+				$(".js-results").append(`<div class="fizz-buzz-item fizz"><span>${i}</span></div>`);
+			}
+			else if (i % 15 === 0) {
+				$(".js-results").append(`<div class="fizz-buzz-item fizzbuzz"><span>${i}</span></div>`);
+			}
+			else {
+				$(".js-results").append(`<div class="fizz-buzz-item"><span>${i}</span></div>`);
+			}
 		}
 	})
-	
 }
+
 fizzBuzzer();
