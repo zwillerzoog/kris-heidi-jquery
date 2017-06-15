bf67e71df4a4ccffd0c2570430fab484919dbfd8
Kristoffer Panahon <krp312@me.com>
2017-06-15 15:34:02 -0400

    making stuff work on local env

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function catClickHandler() {
   $("a.thumbnail").click(function(event) {
     let thumbImgSrc = $(event.currentTarget).find("img").attr("src");
-    let heroImgSrc = $(".hero").find("img").attr("src");
+    // let heroImgSrc = $(".hero").find("img").attr("src");
     $(".hero > img").attr('src', thumbImgSrc);
   });
 }
